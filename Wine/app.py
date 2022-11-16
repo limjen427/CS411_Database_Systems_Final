093d78d0ca90df08136edfcbe30ab690c96aab2e
limjen427 <jylim3@illinois.edu>
2022-11-15 20:46:53 -0600

    stage 4 finish

--- Wine/app.py
+++ Wine/app.py
@@ -1,10 +1,8 @@
 from getpass import getpass
 from mysql.connector import connect, Error
 from flask import Flask, render_template, request, redirect, url_for, session
-
-#from flask_mysqldb import MySQL
-#from flask_cors import CORS, cross_origin
-#import MySQLdb.cursorsenv
+import random
+from tabulate import tabulate
 
 GET_LIMIT = 20
 
@@ -56,113 +54,51 @@ def get_wine_all():
     query = """
             SELECT wineID, name
             FROM Wine
-            LIMIT 20
             """
     return try_query(query)
 
-@app.route('/wine/user', methods = ['GET'])
-def get_recipe_by_user():
-    userID = request.args.get('userID')
-    if userID is None:
-        return "GET request malformed. Must include userID"
-    query = """
-            SELECT rat.ratingID, rec.name, rat.score, rat.review
-	        FROM Rating rat NATURAL JOIN Wine rec
-	        WHERE userID = """ + str(userID)
+@app.route('/wine/search', methods=['GET'])
+def search_wine():
+    search = request.args.get('search')
+    query = "SELECT * FROM Wine WHERE name Like '%" + search + "%'"
     return try_query(query)
 
 # review
-@app.route('/rating/create', methods = ['POST'])
+@app.route('/rating/create', methods = ['GET'])
 def create_rating():
-    ratingID = "NULL" #set to NULL to autogenerate a new ratingID
-    wineID = request.args.get('wineID')
-    score = request.args.get('score')
-    review = request.args.get('review')
+    # ratingID = "NULL" #set to NULL to autogenerate a new ratingID
+    wineID = str(request.args.get('wineID'))
+    score = str(request.args.get('score'))
+    review = str(request.args.get('review'))
     review = "'" + review + "'"
-    userID = request.args.get('userID')
+    userID = str(request.args.get('userID'))
+    ratingID = str(random.randint(405001, 999999999))
     if None in [wineID, score, review, userID]:
         return "POST request malformed. Must include wineID, score, review, userID"
-    query = """INSERT INTO Rating (ratingID, wineID, score, review, userID)
-            VALUES (""" + ratingID + ", " + wineID + "," + score + ", " + review + ", " + userID + ")"
-    return try_query(query)
+    # query = """INSERT INTO Rating (ratingID, wineID, score, review, userID)
+    #         VALUES ((SELECT COUNT(*) FROM Rating) + 405001), wineID, """ + score + "","" + review + "," + userID + ")"
+    query = "INSERT INTO Rating (ratingID, wineID, score, review, userID) VALUES \
+        (" + ratingID + ", " + wineID + ", " + score + ", " + review + ", " + userID + ")"
+    return [try_query(query), 'Thank you for your review! Your ratingID is ' + str(ratingID) + ', and the recorded userID is ' + str(userID)]
 
-@app.route('/rating/read', methods = ['GET'])
-def read_rating():
-    ratingID = request.args.get('ratingID')
-    userID = request.args.get('userID')
-    score = request.args.get('score')
-    wineID = request.args.get('wineID')
-
-    first_flag = False
-    cond = " WHERE "
-
-    if ratingID is not None:
-        cond += "ratingID = " + ratingID
-        first_flag = True
-    
-    if userID is not None:
-        if first_flag == False:
-            first_flag = True
-        else:
-            cond += " AND "
-        cond += "userID = " + userID
-    
-    if score is not None:
-        if first_flag == False:
-            first_flag = True
-        else:
-            cond += " AND "
-        cond += "socre = " + score
-    
-    if wineID is not None:
-        if first_flag == False:
-            first_flag = True
-        else:
-            cond += " AND "
-        cond += "wineID = " + wineID
-
-    query = "SELECT * FROM Rating"
-    if first_flag == False:
-        return try_query(query)
+@app.route('/rating/edit', methods = ['GET'])
+def edit_rating():
+    wineIDtoedit = str(request.args.get('wineID'))
+    ratingIDtoedit = str(request.args.get('ratingID'))
+    scoretoedit = str(request.args.get('score'))
+    reviewtoedit = str(request.args.get('review'))
+    reviewtoedit = "'" + reviewtoedit + "'"
+    userIDtoedit = str(request.args.get('userID'))
+    if len(wineIDtoedit) == 0 or len(ratingIDtoedit) == 0:
+        return "Error. You must include both wineID and ratingID to edit or delete your review!"
+    if len(scoretoedit) + len(reviewtoedit) - 2 + len(userIDtoedit) == 0:
+        query = "DELETE FROM Rating WHERE ratingID = " + ratingIDtoedit
+        return [try_query(query), 'Your review is deleted!']
     else:
-        print(query + cond)
-        return try_query(query + cond)
-
-
-@app.route('/rating/update', methods = ['PUT'])
-def update_rating():
-    ratingID = request.args.get('ratingID')
-    if ratingID is None:
-        return "UPDATE queries must contain a ratingID."
-    score = request.args.get('score')
-    review = request.args.get('review')
-    # review = review
-
-    first_flag = False
-    fragment = " SET "
-    if score is not None:
-        fragment += "score = " + score
-        first_flag = True
-    
-    if review is not None:
-        if first_flag == False:
-            first_flag = True
-        else:
-            fragment += ", "
-        fragment += "review = " + review
-    
-    if first_flag == False:
-        return "UPDATE queries must contain at least 1 value to update."
-    query = "UPDATE Rating" + fragment + " WHERE ratingID = " + ratingID
-    return try_query(query)
-
-@app.route('/rating/delete', methods = ['DELETE'])
-def delete_rating():
-    ratingID = request.args.get('ratingID')
-    if ratingID is None:
-        return 'DELETE request malformed. Must include ratingID'
-    query = 'DELETE FROM Rating WHERE ratingID = ' + ratingID
-    return try_query(query)
+        query = "UPDATE Rating SET wineID = " + wineIDtoedit + \
+            ", score = " + scoretoedit + ", review = " + reviewtoedit + ", userID = " + userIDtoedit + " WHERE ratingID = " + ratingIDtoedit
+        return [try_query(query), 'Your review is updated! The reviewID is ' + ratingIDtoedit + ' and the recorded userID is ' + userIDtoedit]
+    return None
 
 @app.route('/wine/avgRating', methods = ['GET'])
 def get_avgRating_wine():
@@ -175,7 +111,6 @@ def get_avgRating_wine():
                 GROUP BY ux.userID
             )
             GROUP BY wineID
-            LIMIT 20
             """
     return try_query(query)
 
@@ -189,7 +124,6 @@ def get_cheap_best_wine():
                 GROUP BY wineID
             ) as sub ON w.wineID = sub.wineID
             WHERE w.price < 20 AND w.points >= 90 AND sub.avgScore >= 4
-            LIMIT 40
             """
     return try_query(query)
 
